78a275768923ca525480862f1f4ca5f3321e0e70
nataliakuksa <78843901+nataliakuksa@users.noreply.github.com>
2021-10-20 02:21:47 -0700

    Update myTest.test.js

--- myTest.test.js
+++ myTest.test.js
@@ -1,4 +1,4 @@
-const message = require("./message");
+const message = require("./myTest");
 describe("My message", () => {
   test("works", () => {
     expect(messaage.message()).toEqual("TestingCiCd");
